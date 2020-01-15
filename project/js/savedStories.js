8352fb1d2633a0eee638167a8cf5980310112c97
Laura de Jong <lauraesmee@hotmail.com>
2020-01-15 09:47:52 +0100

    Added comments to savedstories.js

--- project/js/savedStories.js
+++ project/js/savedStories.js
@@ -2,14 +2,20 @@
 //https://css-tricks.com/making-css-animations-feel-natural/
 //https://www.w3schools.com/howto/howto_js_remove_class.asp
 //https://catalin.red/removing-an-element-with-plain-javascript-remove-method/
+
+// Haalt de elementen op met onclick="removeElement(this)" in de html
 function removeElement(element) {
+    // haalt de elementen op met de class saved-story van het element this.
     var savedstory = element.getElementsByClassName('saved-story')[0];
+    // voegt de class jump toe aan saved-story
     savedstory.classList.add('jump');
     setTimeout(function() {
+        // class wordt na 1000 ms verwijdert
         savedstory.classList.remove('jump');
     }, 1000);
 
     setTimeout(function() {
+        // element wordt na 2000ms verwijdert
         element.remove();
     }, 2000);
 

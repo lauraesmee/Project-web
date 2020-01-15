6a5d551b969fcce433c1fa0c0cb79f059b367946
Laura de Jong <lauraesmee@hotmail.com>
2020-01-15 09:54:23 +0100

    Addes comments to modal.js

--- project/js/modal.js
+++ project/js/modal.js
@@ -1,12 +1,15 @@
 function validateEmailForgottenPassword(emailField) {
+    // e-mailadres mag alleen letters, cijfers, punten, of lage streepjes hebben. Moet een @ bevatten en een .
     var reg = /^([A-Za-z0-9_\-\.])+\@([A-Za-z0-9_\-\.])+\.([A-Za-z]{2,4})$/;
     var sentBtn = document.getElementsByClassName("sent")[0];
     if (reg.test(emailField.value) == false) {
+        //als email invalide is, dan kleurt de balk rood en wordt de verzendbutton gedisabled.
         emailField.style.backgroundColor = "rgb(255, 149, 132)";
         sentBtn.disabled = true;
         return false;
     }
     else {
+        // als email valide is, kleurt de balk groen en is de button niet meer disabled.
         emailField.style.backgroundColor = "rgb(210, 255, 197)";
         sentBtn.disabled = false;
         return true;
@@ -14,6 +17,8 @@ function validateEmailForgottenPassword(emailField) {
 }
 
 //https://www.w3schools.com/howto/howto_css_modals.asp
+
+//Modal verschijnt wanneer de gebruiker op wachtwoord vergeten heeft geklikt. Hier staat de input in om je emailadres in te vullen voor je vergeten wachtwoord.
 var modal = document.getElementById("myModal");
 var btn = document.getElementById("myBtn");
 var span = document.getElementsByClassName("close")[0];
